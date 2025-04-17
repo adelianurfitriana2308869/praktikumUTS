4526c048832dcdcbd07ee042903961f171148dcd
Adelia Nur Fitriana <adeliaanf@gmail.com>
2025-04-17 08:58:09 +0700

    Update Main.java

--- nomo4uts/Main.java
+++ nomo4uts/Main.java
@@ -10,7 +10,7 @@ package nomo4uts;
  */
 public class Main {
     public static void main(String[] args) {
-        // Objek pertama
+        
         LimasSegiEmpat limas1 = new LimasSegiEmpat();
         limas1.setLuasAlas(1);
         limas1.setLuasSelubung(1);
@@ -19,13 +19,13 @@ public class Main {
                 + ". Luasnya : " + String.format("%.2f", limas1.getLuas()) +
                 ", sedangkan volumenya : " + String.format("%.1f", limas1.getVolume()));
 
-        // Objek kedua
+        
         LimasSegiEmpat limas2 = new LimasSegiEmpat(30, 40, 50);
         System.out.println("Limas segi empat dengan luas alas : " + 30 + ", luas selubung limas : " + 40 + " dan tinggi : " + 50
                 + ". Luasnya : " + String.format("%.1f", limas2.getLuas()) +
                 ", sedangkan volumenya : " + String.format("%.1f", limas2.getVolume()));
 
-        // Objek ketiga
+        
         LimasSegiEmpat limas3 = new LimasSegiEmpat(25, 35, 45);
         System.out.println("Limas segi empat dengan luas alas : " + 25 + ", luas selubung limas : " + 35 + " dan tinggi : " + 45
                 + ". Luasnya : " + String.format("%.2f", limas3.getLuas()) +
